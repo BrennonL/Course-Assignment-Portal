03eb45a71d09dee7e0116adbebee261436c06ce4
BrennonL <104086501+BrennonL@users.noreply.github.com>
2022-10-01 23:19:34 -0600

    I added the navagation bar

---
+++ Week03/Scripts/Scripts03.js
@@ -0,0 +1,9 @@
+const d = new Date();
+let year = d.getFullYear();
+let lastModified = new Date(document.lastModified);
+let newLastModified = Date.parse(document.lastModified);
+
+
+let coppyWrite = `<p>&copy ${year}| Brennon Laney|</p>
+<p>Last modified: ${lastModified}</p>`
+document.querySelector("footer").innerHTML = coppyWrite;
