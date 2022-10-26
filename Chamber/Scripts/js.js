a520b2df83e86d65f8ff084b7e897a3412bfda04
BrennonL <104086501+BrennonL@users.noreply.github.com>
2022-10-26 11:18:05 -0600

    fixed bugs

--- Chamber/Scripts/js.js
+++ Chamber/Scripts/js.js
@@ -7,7 +7,7 @@ temperature = 3.5;
 let f = 35.74 + (0.6215 * temperature) - (35.75 * windSpeed ^ 0.16) + (0.4275 * temperature * windSpeed ^ 0.16);
 
 let temperatureText = "Temperature:" + f;
-let actuatlTemperatureText = "Wind Speed:" + windSpeed + "Temperature:" + temperature;
+let actuatlTemperatureText = "Wind Speed:" + windSpeed + "  Temperature:" + temperature;
 
 document.querySelector('#weatherTxt').innerHTML = temperatureText;
 
